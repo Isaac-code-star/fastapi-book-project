94114ddf0cb5f5343f81603395839995945196f3
Isaac Onovuakpo <onovuakpoisaac@gmail.com>
2025-02-11 22:22:50 +0100

    get books my id and Add CI pipeline

--- api/routes/books.py
+++ api/routes/books.py
@@ -60,3 +60,18 @@ async def update_book(book_id: int, book: Book) -> Book:
 async def delete_book(book_id: int) -> None:
     db.delete_book(book_id)
     return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=None)
+
+#getting book by id
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+async def get_book(book_id: int) -> Book:
+    if book_id not in db.books:
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND,
+            content={"message": f"Book with id {book_id} not found"})
+    
+    return db.get_book(book_id)
+
+#if book dont exist return 404
+# @router.get("/{book_id}", response_model=Book, status_code=status.HTTP_404_NOT_FOUND)
+# async def get_book(book_id: int) -> Book:
+#     return db.get_book(book_id)
